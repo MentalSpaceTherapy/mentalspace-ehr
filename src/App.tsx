21ed0821b3a58d0b18e36de552cf7ce8cf7065b2
Elize Joseph <lelejoseli@gmail.com>
2025-03-31 12:48:47 -0400

    Initial setup for GitHub Pages

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to MentalSpace EHR</h1>
+        <p>
+          Your Electronic Health Records System
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
